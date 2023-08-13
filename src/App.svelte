d6d7f6fec364dbb1af171bf5f2ef482062084627
James Daza <jamesrdaza@gmail.com>
2023-08-13 01:17:03 -0700

    Initial Home Page

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,41 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import Navbar from "./components/Navbar.svelte"
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <Navbar />
+  <div class="content">
+    <div class="upload">
+      <button id="UploadButton">Upload an Image</button>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  main {
+    min-height: 100vh;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  
+  .content {
+    height: calc(100vh - 60px);
+    grid-column: span 12;
+    
+    display: flex;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+  
+  .upload {
+    height: 35%;
+    width: 100%;
+    display: flex;
+    
+    justify-content: center;
+    align-items: center;
   }
-  .read-the-docs {
-    color: #888;
+  
+  #UploadButton {
+   height: 30px; 
   }
 </style>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,41 @@
+<script>
+
+</script>
+
+<nav>
+    <div class="leftNav">
+            <a style="margin-left: 15px;" href="/"><li>fotodumper</li></a>
+            <a href="/"><li>Home</li></a>
+    </div>
+    <div class="rightNav">
+        <a style="margin-right: 15px;" href="/"><li>Profile</li></a>
+    </div>
+</nav>
+
+<style>
+    nav {
+        height: 60px;
+        display: flex;
+        grid-column: span 12; 
+    }
+    
+    li {
+        list-style-type: none;
+    }
+    
+    .leftNav, .rightNav{
+        height: 100%;
+        width: 100%;
+        display: flex;
+        gap: 15px;
+        align-items: center;
+    }
+    
+    .leftNav {
+        justify-content: left;
+    }
+
+    .rightNav {
+        justify-content: right;
+    }
+</style>
